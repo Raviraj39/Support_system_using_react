555ccf0282e4a0544c2344c0a79287f65f2f7339
Raviraj39 <rravirajbujawade@gmail.com>
2024-04-10 02:13:11 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
@@ -13,7 +13,6 @@ function App() {
    <Router>
             <Routes>
 
-              {/* <Route exact path="/" element={<Main/>}/> */}
               <Route exact path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/home" element={<Home />} />
